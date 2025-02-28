4921aecd59bcc955b006d3a781661a493457b19a
anavasynth <vadimgonchariuk@gmail.com>
2025-02-28 13:48:03 +0200

    add admin page

--- app.py
+++ app.py
@@ -39,6 +39,10 @@ TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 def index():
     return render_template("index.html")
 
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
 
 def send_telegram_message(chat_id, message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
